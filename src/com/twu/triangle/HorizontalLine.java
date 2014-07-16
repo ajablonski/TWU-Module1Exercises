06dcdb1eed29fb58d7b42f1492f52b9a721d13ea
Alex Jablonski <alexmjablonski@gmail.com>
2014-07-16 16:19:41 -0500

    Add HorizontalLine

---
+++ src/com/twu/triangle/HorizontalLine.java
@@ -0,0 +1,23 @@
+package com.twu.triangle;
+
+
+public class HorizontalLine {
+    public static void main(String[] args) {
+        HorizontalLine hl = new HorizontalLine(3);
+        System.out.println(hl);
+    }
+
+    private int size;
+    private StringBuilder sb = new StringBuilder();
+
+    public HorizontalLine(int size) {
+        this.size = size;
+    }
+
+    public String toString() {
+        for (int i = 0; i < size; i++) {
+            sb.append("*");
+        }
+        return sb.toString();
+    }
+}
